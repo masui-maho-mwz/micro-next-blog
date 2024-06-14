cfe46a0aeb344c814ee3ebee391d5f441319dacb
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-14 14:05:05 +0900

    fix: URLから取得したparamsの型の処理を修正

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { useBlogs } from '@/hooks/use-blogs';
+import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
 const BlogHome = () => {
   const { getBlogById, error } = useBlogs();
-  const { id } = useParams();
-  const blog = getBlogById(id as string);
+  const params = useParams();
+  const blogId = typeof params.id === 'string' ? params.id : '';
+  const blog = getBlogById(blogId);
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -16,10 +18,15 @@ const BlogHome = () => {
     return <div>Blog not found</div>;
   }
 
+  const { title, body, createdAt, updatedAt } = blog;
+
   return (
     <div>
-      <h3>{blog.title}</h3>
-      <div>{blog.body}</div>
+      <h3>{title}</h3>
+      <div>{body}</div>
+      <div>{createdAt}</div>
+      <div>{updatedAt}</div>
+      <Link href="../../blogs">戻る</Link>
     </div>
   );
 };
